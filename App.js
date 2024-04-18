30cff3d942a3eb96eef4a517015a987a4d9600f5
Andrew Zerdelian <121677421+AndrewZerdelian@users.noreply.github.com>
2024-04-18 17:14:34 +0200

    publish

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import ToDoScreen from "./SRC/screens/ToDoScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>APP Component hel </Text>
+    <View>
+      <View>
+        <ToDoScreen />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
